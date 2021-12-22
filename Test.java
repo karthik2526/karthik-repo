fe1bbcad153a349a35bc57b8be3c822b68e122f7
karthik2526 <47047521+karthik2526@users.noreply.github.com>
2021-12-22 11:10:36 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test.java
+{
+
+pblic Test(String msg){
+  System.out.println(msg + " Test class is initiated");
+}
+
+public static void main(String args[]){
+    
+    Test test = new Test("Welcome!!");
+
+}
+}
